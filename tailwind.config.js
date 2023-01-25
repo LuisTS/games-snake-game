b7b4b249960ef1850fa0a80acef4341ed39a00d4
luists <infoprotoweb@gmail.com>
2023-01-25 11:33:36 +0100

    feat: add tailwind and config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: '',
+  purge: {
+    content: [
+      './src/**/*.{html,ts}',
+    ]
+  },
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  }
+};
